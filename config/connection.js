0a4e220480c6459abbe9bb93781587f23df0ac23
Kakalak <43103025+cdgillis@users.noreply.github.com>
2019-03-09 14:31:30 -0500

    another upload

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,23 @@
-var connection = mysql.createConnection({
+var mysql = require("mysql");
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
-    // Your port; if not 3306
     port: 3306,
-    // Your username
     user: "root",
-    // Your password
     password: "hellojello",
     database: "burgers_db"
   });
 
-connection.connect(function (err) {
-    if (err)throw err;
-    console.log("Connected to db");
-})
+};
 
-  module.exports = connection;
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected to db");
+});
+
+module.exports = connection;
